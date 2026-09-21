a2e74fce3a847fda9c55f88773adb850eb22b9c4
Holdonchik <katenka90@inbox.ru>
2026-09-21 09:56:43 +0300

    websocket

--- websocket_users_client.py
+++ websocket_users_client.py
@@ -6,10 +6,13 @@ async def client():
     """Connect to the WebSocket server, send a message and receive responses."""
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        message = "Привет, сервер!"
-        await websocket.send(message)
-        for _ in range(5):
-            response = await websocket.recv()
-            print(f"Server response: {response}")
+        messages = ["Привет, сервер!", "И снова привет, сервер!", "Как дела, сервер?"]
+
+        for message in messages:
+            await websocket.send(message)
+            for _ in range(5):
+                response = await websocket.recv()
+                print(f"Server response: {response}")
+
 
 asyncio.run(client())
