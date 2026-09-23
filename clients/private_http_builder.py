720cd930eb09dc86e96c266cf81912c6efd1d2fa
Holdonchik <katenka90@inbox.ru>
2026-09-23 17:45:36 +0300

    Рефакторинг и подготовка к написанию API автотестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,16 +1,21 @@
+from functools import lru_cache
+
 from httpx import Client
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
 class AuthenticationUserSchema(BaseModel):
-    """Describes user authentication data"""
+    """Describes user authentication data."""
+    model_config = ConfigDict(frozen=True)
+
     email: EmailStr
     password: str
 
 
+@lru_cache
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     """
     Function creates httpx.Client instance with user authentication.
---
+++ tests/authentication/test_authentication.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+import pytest
+
+from clients.authentication.authentication_client import AuthenticationClient
+from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from fixtures.users import UserFixture
+from tools.assertions.authentication import assert_login_response
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+
+
+@pytest.mark.regression
+@pytest.mark.authentication
+class TestAuthentication:
+    def test_login(self, function_user: UserFixture, authentication_client: AuthenticationClient):
+        user_auth_data = LoginRequestSchema(email=function_user.email, password=function_user.password)
+        response = authentication_client.login_api(request=user_auth_data)
+        response_data = LoginResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(actual=response.status_code, expected=HTTPStatus.OK)
+        assert_login_response(response=response_data)
+        validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
