a92f74ee91a3adc9ffe6c7f74a492a54fe271e33
Holdonchik <katenka90@inbox.ru>
2026-09-23 10:31:18 +0300

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,15 @@ import pytest
 from pydantic import BaseModel, EmailStr
 
 from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
 
+# region Models to use in fixtures
 class UserFixture(BaseModel):
-    """Model for aggregating data returned by the `function_user` fixture """
+    """Model for aggregating user data for 'function_user' fixture."""
     request: CreateUserRequestSchema
     response: CreateUserResponseSchema
 
@@ -19,23 +22,37 @@ class UserFixture(BaseModel):
     def password(self) -> str:
         return self.request.password
 
+    @property
+    def authentication_user(self) -> AuthenticationUserSchema:
+        return AuthenticationUserSchema(email=self.email, password=self.password)
 
+# endregion
+
+# region Get client fixtures
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
     return get_authentication_client()
-
 
 @pytest.fixture
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
 
+@pytest.fixture
+def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
+    return get_private_users_client(user=function_user.authentication_user)
+
+# endregion
+
+# region Test data fixtures
 '''
 NOTE: Структура именования {scope}_{сущность} позволяет легко управлять разными уровнями фикстур 
 (function_user when scope="function", class_user when scope="class" etc).
-NOTE2: Удаление данных после теста делает дебаг сложнее, поэтому в данном случае мы его избегаем.
+NOTE2: Удаление данных после теста делает дебаг сложнее, поэтому в случае function_user мы его избегаем.
 '''
 @pytest.fixture()
 def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response = public_users_client.create_user(request)
     return UserFixture(request=request, response=response)
+
+# endregion
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema, \
+    UserSchema
 from tools.assertions.base import assert_equal
 
 
@@ -14,3 +15,27 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Verifies that user's id, email, last_name, first_name, middle_name have expected values.
+
+    :param actual: Actual id, email, last_name, first_name, middle_name.
+    :param expected: Expected id, email, last_name, first_name, middle_name.
+    :raises AssertionError: If at least one value does not match.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+def assert_get_user_response( get_user_response: GetUserResponseSchema, create_user_response: CreateUserResponseSchema):
+    """
+    Verifies that user data in get user/get user me response equals data in create user response.
+
+    :param get_user_response: Get user/get user me response data.
+    :param create_user_response: Create user response data.
+    :raises AssertionError: If responses do not match.
+    """
+    assert_user(actual=get_user_response.user, expected=create_user_response.user)
