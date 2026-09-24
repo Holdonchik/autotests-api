2378c04c12a22bbae5398c89bc114cc8ee7e0222
Holdonchik <katenka90@inbox.ru>
2026-09-24 16:11:35 +0300

    Практикуемся в написании API автотестов. Часть 1 Практика написания негативных API автотестов для работы с файлами

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -126,10 +126,10 @@ def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorRespo
                 input=actual.details[0].input,
                 context={
                     "error": "invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], "
-                             f"found `{actual.details[0].input[0]}` at 1"
+                             "found `i` at 1"
                 },
                 message="Input should be a valid UUID, invalid character: expected an optional prefix of `urn:uuid:` "
-                        f"followed by [0-9a-fA-F-], found `{actual.details[0].input[0]}` at 1",
+                        "followed by [0-9a-fA-F-], found `i` at 1",
                 location=["path", "file_id"]
             )
         ]
