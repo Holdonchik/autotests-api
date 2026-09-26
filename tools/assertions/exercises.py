8e7d49cd5b33b47e04cf93132297386854c8b11f
Holdonchik <katenka90@inbox.ru>
2026-09-26 16:50:35 +0300

    Практикуемся в написании API автотестов. Часть 3. Создание задания. Получение задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -11,7 +12,7 @@ def assert_create_exercise_response(
 
     :param request: Initial create exercise request.
     :param response: API response with created exercise data.
-    :raises AssertionError: if at least one of the fields does not match.
+    :raises AssertionError: If at least one of the values does not match.
     """
     assert_equal(response.exercise.title, request.title, "title")
     assert_equal(response.exercise.course_id, request.course_id, "course_id")
@@ -20,3 +21,33 @@ def assert_create_exercise_response(
     assert_equal(response.exercise.order_index, request.order_index,"order_index")
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Verifies that actual exercise data match the expected values.
+
+    :param actual: Actual exercise data.
+    :param expected: Expected exercise data.
+    :raises AssertionError: If at least one of the values does not match.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+def assert_get_exercise_response(
+        get_exercise_response: GetExerciseResponseSchema,
+        create_exercise_response: CreateExerciseResponseSchema):
+    """
+    Verifies that exercise data in get exercise response equals data in create exercise response.
+
+    :param get_exercise_response: Get exercise response data.
+    :param create_exercise_response: Create exercise response data.
+    :raises AssertionError: If responses do not match.
+    """
+    assert_exercise(actual=get_exercise_response.exercise, expected=create_exercise_response.exercise)
